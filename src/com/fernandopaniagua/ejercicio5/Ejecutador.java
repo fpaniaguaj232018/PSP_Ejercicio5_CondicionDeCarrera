22efb4f29ff8071f6e63b4d1b7c878c59559b0a0
fpaniaguaj232018 <fpaniaguaj232018@gmail.com>
2018-11-05 17:12:06 +0100

    Comentarios

--- src/com/fernandopaniagua/ejercicio5/Ejecutador.java
+++ src/com/fernandopaniagua/ejercicio5/Ejecutador.java
@@ -1,6 +1,11 @@
 package com.fernandopaniagua.ejercicio5;
 
 public class Ejecutador {
+    /*
+        CON UN NÚMERO SUFICIENTE DE HILOS Y DE MÁXIMO POR HILOS 
+        PODREMOS OBSERVAR QUE EL RESULTADO FINAL NO ES EL ESPERADO
+        DEBIDO A UNA CONDICIÓN DE CARRERA.
+    */
     private static final long NUM_HILOS = 10;
     private static final int MAXIMO_POR_HILO = 100000;
     public static void main(String[] args) {
